c5b1e82ac59ae46471f563013d6656e9c87abd31
Anne <anne.dupin91@gmail.com>
2021-04-27 09:53:11 +0200

    First

---
+++ script.js
@@ -0,0 +1,4 @@
+$('ul#list li:first').css("color", "red");
+$('ul#list li:nth-child(2n)').css("color", "red");
+$('ul#list li:nth-child(3n)').css('color', 'red');
+$('ul#list li:last').css('color', 'red');
